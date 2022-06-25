1aa6d251028b126757948f04f86d9ac494950ea0
Ian <iancwm@github.com>
2022-06-25 12:20:58 +0000

    Cleaning up documentation for assignment

--- nea_assignment.py
+++ nea_assignment.py
@@ -1,22 +1,19 @@
-
-import pandas as pd
 import gcsfs
 
-from scipy.spatial import distance
-
-from tqdm import tqdm
-
+import pandas as pd
 import geopandas as gpd
-
-from src import assignment
+from scipy.spatial import distance
+from tqdm import tqdm
 
 import warnings
 warnings.filterwarnings('ignore')
 
-def get_grid_data(gridfile:str):
-    '''Read the shpfile containing Singpaore grid data
+from src import assignment
+
+def get_grid_data(gridfile:str) -> type(gpd.GeoDataFrame):
+    '''Read the shpfile containing Singapore grid data
     Args:
-        gridfile:str:Path to gridfile
+        gridfile:str:   Path to gridfile
     Returns
         geopandas.DataFrame with following schema
         - grid_num 
@@ -35,7 +32,7 @@ def get_grid_data(gridfile:str):
     return grids_df
 
 
-def query_nea_metadata(measure:str, query:str, project_id='ml-eng-cs611-group-project',dataset_id='taxi_dataset_reference'):
+def query_nea_metadata(measure:str, query:str, project_id:str='ml-eng-cs611-group-project',dataset_id:str='taxi_dataset_reference')->type(pd.DataFrame):
     '''Query NEA BigQuery for metadata
     Args:
         measure:            rainfall, relative-humidity or air-temperature
@@ -56,8 +53,8 @@ def query_nea_metadata(measure:str, query:str, project_id='ml-eng-cs611-group-pr
     return pd.read_gbq(sql, project_id=project_id)
 
 
-def query_nea_items(measure:str, query:str, project_id='ml-eng-cs611-group-project',dataset_id='taxi_dataset_reference'):
-    '''Query NEA BigQuery for metadata
+def query_nea_items(measure:str, query:str, project_id:str='ml-eng-cs611-group-project',dataset_id:str='taxi_dataset_reference')->type(pd.DataFrame):
+    '''Query NEA BigQuery for values
     Args:
         measure:            rainfall, relative-humidity or air-temperature
         query_timestamp:    i.e. 2022-06-01 13:15:00
@@ -66,6 +63,7 @@ def query_nea_items(measure:str, query:str, project_id='ml-eng-cs611-group-proje
     Returns:
         pandas.DataFrame containing metadata for selected measure
     '''
+
     table_dict={'rainfall':'rainfall-items','relative-humidity':'relative-humidity-items','air-temperature':'air-temperature-items'}
 
     sql = f"""
@@ -77,60 +75,7 @@ def query_nea_items(measure:str, query:str, project_id='ml-eng-cs611-group-proje
     return pd.read_gbq(sql, project_id=project_id)
 
 
-def query_nea_view(measure:str, query:str, project_id='ml-eng-cs611-group-project',dataset_id='taxi_dataset_views'):
-    '''Query NEA BigQuery for metadata
-    Args:
-        measure:            rainfall, relative-humidity or air-temperature
-        query_timestamp:    i.e. 2022-06-01 13:15:00
-        project_id:         Google Cloud project_id
-        dataset_id:         Google Cloud dataset_id
-    Returns:
-        pandas.DataFrame containing metadata for selected measure
-    '''
-    table_dict={'rainfall':'view-rainfall','relative-humidity':'view-relative-humidity','air-temperature':'view-air-temperature'}
-
-    sql = f"""
-    SELECT *
-    FROM `{dataset_id}.{table_dict[measure]}`
-    WHERE timestamp = '{query}'
-    """
-
-    return pd.read_gbq(sql, project_id=project_id)
-
-
-def assign_grids(grids_df, df_metadata, df_items, grid_nums):
-    '''
-    Arguments
-    grids_df: dataframe that contains grid numbers and their centroid latlon    
-    df_metadata: dataframe with station metadata i.e. latitude and longitude
-    df_: dataframe that contains stn latlon
-    ts: timestamp
-    grid_nums: list of unique grid numbers
-    '''
-    
-    df_metadata['latlon']=df_metadata[['longitude','latitude',]].apply(tuple,axis=1)
-    df_metadata.index=df_metadata['station']
-    assignment={}
-
-    for i in tqdm(range(len(grid_nums)),desc='Grids assigned'): # for each grid_num
-        grid_coordinates = grids_df.iloc[i]['latlon'] # latlon of row i grid_num
-        
-        distances = df_metadata['latlon'].apply(lambda x: distance.euclidean(x,grid_coordinates))
-        distance_sorted = distances.sort_values()
-        # print(distance_sorted)
-        for station in distance_sorted.index:
-            if len(df_items[df_items['station_id']==station])>0: # there is a value
-                # print(f"Assigning station {station} to grid {i}")
-                assignment[grids_df.iloc[i]['grid_num']]=station
-                break
-        
-            else:
-                continue           
-    
-    return assignment
-
-
-def assign_measure(query,measure,grids_df):
+def assign_measure(query:str,measure:str,grids_df:type(gpd.GeoDataFrame))->type(pd.DataFrame):
     '''Generate ranked assignment of stations to grids sorted by Euclidean distance
     Args:
         query:str:                      Timestamp to query databases e.g. '2022-06-01 13:15:00'
@@ -142,10 +87,10 @@ def assign_measure(query,measure,grids_df):
             grid_num:       Unique integer representing a grid in grids_df
             date_active:    Date which mapping is valid
             date_inactive:  Date which mapping becomes invalid
-            rank:           Integer declaring position relating to distance of station from grid
-            station_id:     Unique identifier for station
-            measure:        relative-humidity, rainfall or air-temperature
+            rank:           Integer declaring position relating to distance of station_id from grid_num
+            station_id:     Unique identifier for station            
     '''
+
     df_metadata=query_nea_metadata(measure=measure,query=query)
     df_metadata['latlon']=df_metadata[['longitude','latitude',]].apply(tuple,axis=1)
     df_metadata.index=df_metadata['station']
@@ -160,47 +105,24 @@ def assign_measure(query,measure,grids_df):
         assignment[grids_df.iloc[i]['grid_num']]=ranked
 
     measure_df=pd.DataFrame(assignment).T.reset_index().melt(id_vars='index')
-    measure_df.rename(columns={'index':'grid_num','variable':'rank','value':'station_id'},inplace=True)
-    measure_df['measure']=measure
+    measure_df.rename(columns={'index':'grid_num','variable':'rank','value':'station_id'},inplace=True)    
     measure_df['date_active']=pd.to_datetime(query)
     measure_df['date_inactive']=pd.to_datetime('2050-12-31 00:00:00')
-    measure_df = measure_df[['grid_num','date_active','date_inactive','rank','station_id','measure']]
+    measure_df['rank']=measure_df['rank'].astype(int)
+    measure_df = measure_df[['grid_num','date_active','date_inactive','rank','station_id']]
     return measure_df
-
-
-def assign_grids_v2(df_metadata, grids_df, query):
-    '''
-    Arguments:    
-        df_metadata: dataframe with station metadata i.e. latitude and longitude
-        df_: dataframe that contains stn latlon
-        query: timestamp
-    grid_nums: list of unique grid numbers
-    '''
-    measures=['air-temperature','rainfall','relative-humidity']
-    df_metadata['latlon']=df_metadata[['longitude','latitude',]].apply(tuple,axis=1)
-    df_metadata.index=df_metadata['station']
-    assignment = {}
-    for measure in measures:
-        print(f"Assigning stations for [{measure}]")
-        result=assign_measure(query,measure,grids_df)
-        assignment[measure]=result
-
-    assignment_df=pd.concat(assignment).reset_index()
-    assignment_df.drop(['level_0','level_1'],axis=1,inplace=True)
-    
-    return assignment_df
 
 
 if __name__ == '__main__':
     import argparse
     
-    parser = argparse.ArgumentParser(description='Generates ')
+    parser = argparse.ArgumentParser(description='Ranks stations by Euclidiean distance from the centroid of each grid for the purposes of assigning a weather station to a grid')
     parser.add_argument('--project','-p', nargs='?', default='ml-eng-cs611-group-project', type=str, help='GCP project name i.e. ml-eng-cs611-group-project')
     parser.add_argument('--bucket','-b', nargs='?', default='ml-eng-cs611-group-project-nea', type=str, help='GCS bucket name i.e. ml-eng-cs611-group-project-nea')
-    parser.add_argument('--dataset_id','-d', nargs='?', default='taxi_dataset_reference', type=str, help='GBQ dataset ID i.e. taxi_dataset')
+    parser.add_argument('--dataset_id','-d', nargs='?', default='taxi_dataset_reference', type=str, help='GBQ dataset ID i.e. taxi_dataset for live data, taxi_dataset_reference for reference data.')
     parser.add_argument('--table_id','-t', nargs='?', default='assignment-station', type=str, help='GBQ table ID i.e. assignment-taxi')
     parser.add_argument('--gridfile','-g', nargs='?', default='./updated codes/filter_grids_2/filter_grids_2.shp', type=str, help='Path to gridfiles (static data)')
-    parser.add_argument('--query','-q', type=str, help='Timestamp to assign i.e. 2022-06-01 13:15:00')    
+    parser.add_argument('--query','-q', type=str, help='Timestamp to assign i.e. 2022-06-01 13:15:00')
     args = parser.parse_args()
     
     project = args.project
@@ -221,21 +143,10 @@ if __name__ == '__main__':
     grid_nums = list(grids_df['grid_num'].unique())
 
     ### Section 2: Assign NEA stations to grid
-    assignment = {measure:[] for measure in measures}
+    
     for measure in measures:
-        print(f"Assigning {measure} to grid")
-        df_metadata=query_nea_metadata(measure=measure,query=query)
-        df_items=query_nea_items(measure=measure,query=query)
-        result=assign_grids(grids_df,df_metadata,df_items,grid_nums)
-        assignment[measure]=result
-
-    assignment_df=pd.DataFrame(assignment)
-    assignment_df['date_active']=pd.to_datetime(query)
-    assignment_df['date_inactive']=pd.to_datetime('2050-12-31 00:00:00')
-    assignment_df=assignment_df[['date_active','date_inactive','rainfall','relative-humidity','air-temperature']]
-    assignment_df.reset_index(inplace=True)
-    assignment_df.rename(columns={'index':'grid_num','relative-humidity':'relative_humidity','air-temperature':'air_temperature'},inplace=True)
-
-    ### Section 3: Write to Bigquery
-    print("Uploading results to Bigquery...")
-    assignment_df.to_gbq('.'.join([dataset_id,table_id]),project,if_exists='append')
+        print(f"Assigning stations for [{measure}]")
+        result=assign_measure(query,measure,grids_df)    
+        table_id=f'assignment-station-{measure}'
+        print(f"Writing to table {dataset_id}.{table_id}")
+        result.to_gbq('.'.join([dataset_id,table_id]),project,if_exists='append')
--- taxi_assignment.py
+++ taxi_assignment.py
@@ -1,30 +1,27 @@
-
-import pandas as pd
 import gcsfs
 
+import pandas as pd
+import geopandas as gpd
 from scipy.spatial import distance
-from datetime import datetime,timedelta
-
-import math
-from collections import Counter
-
 from tqdm import tqdm
 
-import geopandas as gpd
-
-from src import assignment
-
+import math
+from datetime import datetime,timedelta
+from collections import Counter
 import warnings
 warnings.filterwarnings('ignore')
 
-def query_taxi_availability(query:str, project_id='ml-eng-cs611-group-project',dataset_id='taxi_dataset_reference'):
+def query_taxi_availability(query:str, project_id:str='ml-eng-cs611-group-project',dataset_id:str='taxi_dataset_reference')->type(pd.DataFrame):
     '''Query LTA BigQuery for taxi coordinates
     Args:        
         query_timestamp:    i.e. 2022-06-01 13:15:00
         project_id:         Google Cloud project_id
         dataset_id:         Google Cloud dataset_id
     Returns:
-        pandas.DataFrame containing metadata for taxi data
+        pandas.DataFrame containing metadata for taxi data of the schema
+            - timestamp:datetime.timestamp  Timestamp of observation for a taxi
+            - longitude:float               Longitude of a taxi in degrees
+            - latitude:float                Latitude of a taxi in degrees
     '''
     sql = f"""
     SELECT *
@@ -35,16 +32,16 @@ def query_taxi_availability(query:str, project_id='ml-eng-cs611-group-project',d
     return pd.read_gbq(sql, project_id=project_id)
 
 
-def get_grid_data(gridfile:str):
-    '''Read the shpfile containing Singpaore grid data
+def get_grid_data(gridfile:str)->type(gpd.GeoDataFrame):
+    '''Read the shpfile containing Singapore grid data
     Args:
-        gridfile:str:Path to gridfile
+        gridfile:str:   Path to gridfile
     Returns
         geopandas.DataFrame with following schema
-        - grid_num 
-        - intersect
-        - geometry (active geometry)        
-        - latlon (tuple)
+        - grid_num:int                  Unique grid ID in Singapore 
+        - intersect                     
+        - geometry (active geometry)    shapely Objects describing the grid
+        - latlon (tuple)                Tuple of coordinates of the form (longitude,latitude)
     '''
     grids = gpd.read_file(gridfile)
     grids['centroid'] = grids['geometry'].apply(lambda x: x.centroid) # get grids' centroid
@@ -56,19 +53,24 @@ def get_grid_data(gridfile:str):
     return grids_df
 
 
-def assign_taxis(taxi_df,grids):
+def assign_taxis(taxi_df:type(pd.DataFrame),grids)->type(pd.DataFrame):
     '''Numerically determine grid number of each taxi, and counts the number of taxis in each grid
     Args:
         taxi_df:pandas.DataFrame
         grids:geopandas.DataFrame
     Yields:
-        taxi_clean:pandas.DataFrame
+        taxi_clean:pandas.DataFrame containing metadata for taxi data of the schema
+            - timestamp:datetime.timestamp  Timestamp for assignment
+            - grid_num:int                  Unique grid in Singapore GeoDataFrame
+            - taxi_count:float              Number of taxis in grid
     '''
-    def get_grid_longitude(longitude:float):    
+    def get_grid_longitude(longitude:float):
+        '''Maps longitude to a grid'''
         return math.ceil((longitude-103.6)/0.020454545454545583)
 
 
     def get_grid_latitude(latitude:float):
+        '''Maps latitude to a grid'''
         return (13 - math.ceil((latitude -1.208)/0.020538461538461547))*22
     
     longitude_array = taxi_df['longitude'].to_numpy()
@@ -99,17 +101,15 @@ if __name__ == '__main__':
     import argparse
     
     parser = argparse.ArgumentParser(description='Generates ')
-    parser.add_argument('--project','-p', nargs='?', default='ml-eng-cs611-group-project', type=str, help='GCP project name i.e. ml-eng-cs611-group-project')
-    parser.add_argument('--bucket','-b', nargs='?', default='ml-eng-cs611-group-project-nea', type=str, help='GCS bucket name i.e. ml-eng-cs611-group-project-nea')
-    parser.add_argument('--dataset_id','-d', nargs='?', default='taxi_dataset_reference', type=str, help='GBQ dataset ID i.e. taxi_dataset')
+    parser.add_argument('--project','-p', nargs='?', default='ml-eng-cs611-group-project', type=str, help='GCP project name i.e. ml-eng-cs611-group-project')    
+    parser.add_argument('--dataset_id','-d', nargs='?', default='taxi_dataset_reference', type=str, help='GBQ dataset ID i.e. taxi_dataset. For live data, use taxi_dataset')
     parser.add_argument('--table_id','-t', nargs='?', default='assignment-taxi', type=str, help='GBQ table ID i.e. assignment-taxi')
     parser.add_argument('--gridfile','-g', nargs='?', default='./updated codes/filter_grids_2/filter_grids_2.shp', type=str, help='Path to gridfiles (static data)')    
-    parser.add_argument('--query','-q', type=str, help='Timestamp to assign i.e. 2022-06-01 13:15:00')
+    parser.add_argument('--query','-q', type=str, help='Timestamp to start assignment i.e. 2022-06-01 13:15:00')
     parser.add_argument('--batch','-B', nargs='?', default=0, type=int, help='No. of 15 min intervals to batch process')
     args = parser.parse_args()
     
-    project = args.project
-    bucket = args.bucket
+    project = args.project    
     dataset_id = args.dataset_id
     table_id = args.table_id
     gridfile = args.gridfile    
@@ -120,13 +120,11 @@ if __name__ == '__main__':
 
     fs = gcsfs.GCSFileSystem(project=project)
         
-    ### Section 1: Read grid file data and preprocess
+    ### Read grid file data and preprocess
     grids_df = get_grid_data(gridfile)
-    
-    # Get unique grid_num
     grid_nums = list(grids_df['grid_num'].unique())    
 
-    ### Section 3: Assign taxis to grid
+    ### Assign taxis to grid
     query_datetime = datetime.strptime(query,'%Y-%m-%d %H:%M:%S')
 
     date_list=[query_datetime + timedelta(minutes=15*x) for x in range(1+batch)]
@@ -139,5 +137,7 @@ if __name__ == '__main__':
         except:
             print(f"Empty data for {date}")
     consolidated_df = pd.concat(df_list)
+
+    ### Write to BigQuery
     consolidated_df.to_gbq('.'.join([dataset_id,table_id]),project,if_exists='append')
     print(f"Loaded {1+batch} timestamp(s) successfully")
